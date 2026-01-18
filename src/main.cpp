4108d6f913770a920c82577e6aad5df3793b264a
jUnG3 <jUnG3@users.noreply.github.com>
2026-01-18 21:53:36 +0100

    One step closer to getting it

--- src/main.cpp
+++ src/main.cpp
@@ -32,49 +32,62 @@ void printPlayerRecord(unsigned char *data, size_t length) {
 
 static inline void readCompressedActions(::std::ifstream &replayStream, baje::ReplayHeader &header) {
 	char unknown[4] = {0};
-	z_stream strm;
-	/* allocate inflate state */
-	strm.zalloc = Z_NULL;
-	strm.zfree = Z_NULL;
-	strm.opaque = Z_NULL;
-	strm.avail_in = 0;
-	strm.next_in = Z_NULL;
-	auto ret = inflateInit(&strm);
-	if (ret != Z_OK) {
-		return;
-	}
+	
 	bool error = false;
 	for (size_t i = 0; i < header.numberOfCompressedDataBlocks; ++i) {
 		replayStream.read(reinterpret_cast<char *>(&header.sizeOfCompressedDataBlock), sizeof(header.sizeOfCompressedDataBlock));
 		replayStream.read(reinterpret_cast<char *>(&header.sizeOfDecompressedDataBlock), sizeof(header.sizeOfDecompressedDataBlock));
 		replayStream.read(unknown, sizeof(unknown));
+		::std::cout << ::std::dec << header.sizeOfCompressedDataBlock << ", " << header.sizeOfDecompressedDataBlock << ::std::endl;
 		unsigned char *in = new unsigned char[header.sizeOfCompressedDataBlock];
 		unsigned char *out = new unsigned char[header.sizeOfDecompressedDataBlock];
 		replayStream.read(reinterpret_cast<char *>(in), header.sizeOfCompressedDataBlock);
 
+		z_stream strm;
+		/* allocate inflate state */
+		strm.zalloc = Z_NULL;
+		strm.zfree = Z_NULL;
+		strm.opaque = Z_NULL;
+		strm.avail_in = 0;
+		strm.next_in = Z_NULL;
+		auto ret = inflateInit(&strm);
+		if (ret != Z_OK) {
+			error = true;
+
+		}
 		strm.avail_in = header.sizeOfCompressedDataBlock;
 		strm.next_in = in;
+		int r;
+		do {
+		    r = inflate(&strm, Z_SYNC_FLUSH);
 
+		    if (r == Z_NEED_DICT) r = Z_DATA_ERROR;
 
-		while(true) {
-			strm.avail_out = header.sizeOfDecompressedDataBlock;
-			strm.next_out = out;
-			ret = inflate(&strm, Z_SYNC_FLUSH);
+		    if (r != Z_OK && r != Z_STREAM_END) {
+			std::cerr << "Block " << i << ": inflate error r=" << r
+				  << " msg=" << (strm.msg ? strm.msg : "(null)") << "\n";
+			error = true;
+				break;
+		    }
 
-			if  (ret != Z_OK) {
+		    // If output buffer fills but stream not ended, that's a format mismatch vs outSize
+		    if (r != Z_STREAM_END && strm.avail_out == 0) {
+			std::cerr << "Block " << i << ": output buffer too small (outSize=" << header.sizeOfDecompressedDataBlock << ")\n";
 				error = true;
-				::std::cout << "Inflating error " << ret << " (" << strm.msg << ")" << ::std::endl;
-				goto done;
-			}
-			if (ret == Z_STREAM_END) {
-			    std::cout << "stream end reached at block " << i << "\n";
-			    goto done;
-			}
-			auto produced = header.sizeOfDecompressedDataBlock - strm.avail_out;
-			printPlayerRecord(out + 4, produced - 4);
-		}
+				break;
+		    }
 
-	done:
+		    // If input consumed but not ended, we might need more input (shouldn't happen if n is correct)
+		    if (r != Z_STREAM_END && strm.avail_in == 0) {
+			// Some formats rely on Z_SYNC_FLUSH and don't end with STREAM_END; accept exact outSize
+			break;
+		    }
+
+		} while (r != Z_STREAM_END);
+
+		auto produced = header.sizeOfDecompressedDataBlock - strm.avail_out;
+		printPlayerRecord(out + 4, produced - 4);
+
 		delete [] in;
 		delete [] out;
 		inflateEnd(&strm);
@@ -98,10 +111,10 @@ static inline void collectDataFromReplayFile(const ::std::string &filePath) {
 		replayStream.read(reinterpret_cast<char *>(&header.version), sizeof(header.version));
 		::std::cout << "Replay header version:\t0x" << header.version << ::std::endl;
 
-
 		replayStream.seekg(0x002c);
 		replayStream.read(reinterpret_cast<char *>(&header.numberOfCompressedDataBlocks), sizeof(header.numberOfCompressedDataBlocks));
 		::std::cout << "Number of compressed data blocks:\t" << ::std::dec << header.numberOfCompressedDataBlocks << ::std::endl;	
+		replayStream.seekg(header.size);
 		readCompressedActions(replayStream, header);
 
 	} else {
